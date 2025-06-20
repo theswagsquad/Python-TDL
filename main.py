a29bc023daa4c800a6656e63ee01c9a252416417
gbentham06 <gbentham06@gmail.com>
2025-06-20 22:10:02 +0100

    addtask() added
    added addtask() with the correct datatypes as well as imported the task class into main

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from task import task
+import datetime
+
 def main():
     tasklist = []
     run()
@@ -6,6 +9,13 @@ def main():
 def run():
     return
 
+def addTask(tasklst):
+    taskname = input("name of task: ")
+    priority = int(input("priority (1,2,3): "))
+    desc = input("give your task a description: ")
+    date = input("give the due date (dd-mm-yyyy): ")
+    duedate = datetime.strptime(date, '%d-%m-%Y').date()
+    tasklst.append(task(taskname, desc, duedate, False, priority))
 
 
 
