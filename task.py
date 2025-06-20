9c9c638f02db0eb066f00c2b62f5dfad0a4cbfb5
BaconGod0000 <bacongod0000@gmail.com>
2025-06-20 23:12:36 +0100

    buggy ahh

--- task.py
+++ task.py
@@ -1,10 +1,20 @@
 class Task:
     def __init__(self, name: str, desc: str, deadline: datetime, complete: bool, priority: int):
-        self.name = name
-        self.desc = desc
-        self.deadline = deadline
-        self.complete = complete
-        self.priority = priority
+        self.__name = name
+        self.__desc = desc
+        self.__deadline = deadline
+        self.__complete = complete
+        self.__priority = priority
 
     def __str__(self):
-        return f"{self.name} {self.complete}"
+        return f"{self.__name} {self.__complete}"
+
+    def MarkComplete(self):
+        done = input("Is your task complete? (y for complete): ")
+        if done.lower() == "y":
+            self.__complete = True
+        else:
+            self.__complete = False
+
+    def ViewDetailed(self):
+        pass
