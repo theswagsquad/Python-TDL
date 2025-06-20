50e8645fc401a72fead47b2147734ec80fe2aea5
gbentham06 <gbentham06@gmail.com>
2025-06-20 22:39:02 +0100

    fixed datetime formatting in main

--- main.py
+++ main.py
@@ -3,18 +3,29 @@ import datetime
 
 def main():
     tasklist = []
-    run()
+    running = True
+    while running == True:
+        option = input("what would you like to do?")
+        if option == "add task":
+            addTask(tasklist)
+        elif option == "view tasks":
+            viewTasks(tasklist)
 
 
-def run():
-    return
+def viewTasks(tasklst):
+    if len(tasklst) == 0:
+        print("you have no tasks")
+    else:
+        for i,tsk in enumerate(tasklst):
+            print(f"{i}. {tsk}")
+
 
 def addTask(tasklst):
     taskname = input("name of task: ")
     priority = int(input("priority (1,2,3): "))
     desc = input("give your task a description: ")
     date = input("give the due date (dd-mm-yyyy): ")
-    duedate = datetime.strptime(date, '%d-%m-%Y').date()
+    duedate = datetime.datetime.strptime(date, '%d-%m-%Y').date()
     tasklst.append(Task(taskname, desc, duedate, False, priority))
 
 
