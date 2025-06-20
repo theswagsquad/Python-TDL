5a5709b74acb545c972e5fb0218922fa84540c4e
BaconGod0000 <bacongod0000@gmail.com>
2025-06-20 22:02:20 +0100

    Add files via upload
    Created the Task class containing attributes name, description, deadline, un/completed and priority.

---
+++ task.py
@@ -0,0 +1,7 @@
+class Task:
+    def __init__(self, name: str, desc: str, deadline: datetime, complete: bool, priority: int):
+        self.name = name
+        self.desc = desc
+        self.deadline = deadline
+        self.complete = complete
+        self.priority = priority
